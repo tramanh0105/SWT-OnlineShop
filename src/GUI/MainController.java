c27927646a97874289edb47839a242a064713438
Tram Anh <tramanh0105@gmail.com>
2019-05-14 20:16:35 +0200

    Updating Scene Builder Forms and Event Handler

--- src/GUI/MainController.java
+++ src/GUI/MainController.java
@@ -7,6 +7,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class LoginController{
+    @FXML
+  TextField txtPasswort;
+    @FXML
+  TextField txtUsername;
 
     //handle event for button login
     public void login(){
